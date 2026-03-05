4fcfeb33a5ebddde31740fdaca3bb8ea0e37bedc
Daniel Foerster <pydsigner@gmail.com>
2026-03-05 01:07:47 -0600

    Use icons for create folder and upload file actions

--- src/browse.js
+++ src/browse.js
@@ -40,6 +40,12 @@ const svgContextActionIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24"
     '<path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M6 12 h0.01"></path>' +
     '<path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M12 12 h0.01"></path>' +
     '<path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M18 12 h0.01"></path></svg>';
+const svgNewFolderActionIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">' +
+    '<path d="m.5 3 .04.87a2 2 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2m5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19q-.362.002-.683.12L1.5 2.98a1 1 0 0 1 1-.98z"/>' +
+    '<path d="M13.5 9a.5.5 0 0 1 .5.5V11h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V12h-1.5a.5.5 0 0 1 0-1H13V9.5a.5.5 0 0 1 .5-.5"/></svg>';
+const svgUploadActionIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">' +
+    '<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>' +
+    '<path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/></svg>';
 
 const DEFAULT_ICON_RULES = [
     { icon: svgDocumentIcon('IMG', '#c7ddff'), typeMatch: t => t.startsWith('image/'), exts: ['.png', '.jpg', '.jpeg', '.gif', '.webp', '.bmp', '.svg', '.avif'] },
@@ -355,7 +361,8 @@ export class BrowseJS {
             const createBtn = document.createElement('button');
             createBtn.type = 'button';
             createBtn.className = 'brj-action';
-            createBtn.textContent = 'New Folder';
+            createBtn.title = 'New Folder';
+            createBtn.innerHTML = svgNewFolderActionIcon;
             createBtn.addEventListener('click', () => this.handleCreateFolder());
             this.actionsEl.appendChild(createBtn);
         }
@@ -364,7 +371,8 @@ export class BrowseJS {
             const uploadBtn = document.createElement('button');
             uploadBtn.type = 'button';
             uploadBtn.className = 'brj-action';
-            uploadBtn.textContent = 'Upload';
+            uploadBtn.title = 'Upload';
+            uploadBtn.innerHTML = svgUploadActionIcon;
             uploadBtn.addEventListener('click', () => this.handleUpload());
             this.actionsEl.appendChild(uploadBtn);
         }
